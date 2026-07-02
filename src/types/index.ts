91bc71e7c395d1444ebf00f6324c13a2fb377c4c
AndieMonster <computacion@emlider.edu.mx>
2026-07-02 06:52:01 -0600

    Add files via upload

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export interface Value {
+  id: number;
+  title: string;
+  description: string;
+  icon: string;
+  image: string;
+}
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+}
+export interface ExperienceStep {
+  id: number;
+  title: string;
+  description: string;
+}
